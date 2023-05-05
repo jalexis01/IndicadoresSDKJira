81c54d1b9e4596aff7b8cf9bdbec3c57189c1578
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-05-05 16:26:36 -0500

    Test DB access

---
+++ DB/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DB.Data;
+
+Console.WriteLine("Hello, World!");
+
+using (var context = new MqttservicesbdContext()) 
+{
+    Console.WriteLine(context.TbMessages.ToList().Count);
+    foreach(var comands in context.TbMessages.ToList())
+    {
+        Console.WriteLine(comands.FechaHoraEnvioDato );
+
+    }
+}
