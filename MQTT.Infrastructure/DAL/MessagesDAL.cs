8dda13cf07e27b1fa122642ea7675f33600d52ca
Juan Andres Orozco <120572675+AorozcoEys@users.noreply.github.com>
2023-06-05 13:58:02 -0500

    Changed a method in the MessageDAL.cs app

--- MQTT.Infrastructure/DAL/MessagesDAL.cs
+++ MQTT.Infrastructure/DAL/MessagesDAL.cs
@@ -391,6 +391,13 @@ namespace MQTT.Infrastructure.DAL
 
                 if (dtInit == dtEnd)
                 {
+                    sentence = "SELECT TOP(10000) M.fechaHoraLecturaDato, M.fechaHoraEnvioDato, DATEADD(HOUR,-5,HM.CreationDate) [CreationDateLocal], M.idEstacion, " +
+                    "M.idVagon, M.idPuerta, M.codigoEvento, M.estadoAperturaCierrePuertas, M.numeroParada, M.idVehiculo, M.placaVehiculo, M.tipologiaVehiculo, " +
+                    "M.estadoErrorCritico, M.nombreEstacion, M.nombreVagon, M.codigoPuerta, M.numeroEventoBusEstacion, M.tipoTramaBusEstacion, HM.fechaPrimerIntento, " +
+                    "HM.estadoEnvio, HM.estadoEnvioManatee, HM.fechaHoraEnvio, M.versionTrama, M.tipoTrama, M.tramaRetransmitida, HM.trama, M.usoBotonManual, " +
+                    "M.estadoBotonManual, M.porcentajeCargaBaterias, M.ciclosApertura, M.horasServicio, M.tipoEnergizacion, M.velocidaMotor, M.fuerzaMotor, " +
+                    "M.modoOperacion, M.codigoAlarma, M.codigoNivelAlarma, M.tiempoApertura, M.IdHeaderMessage, HM.IdMessageType, M.idRegistro, M.idOperador, M.Id " +
+                    "FROM [Operation].[tbMessages] M INNER JOIN [Operation].tbHeaderMessage HM ON M.IdHeaderMessage = HM.IdHeaderMessage ";
                     where = " ";
                 }
 
