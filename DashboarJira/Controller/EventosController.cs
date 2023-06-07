5d0d467d1310eb4716eaee8b41d09aa6047893e1
Juan Andres Orozco <120572675+AorozcoEys@users.noreply.github.com>
2023-06-07 14:59:17 -0500

    Update some stuff

--- DashboarJira/Controller/EventosController.cs
+++ DashboarJira/Controller/EventosController.cs
@@ -1,5 +1,6 @@
 ﻿using DashboarJira.Model;
 using DashboarJira.Services;
+using MQTT.Infrastructure.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,9 @@ namespace DashboarJira.Controller
 {
     internal class EventosController
     {
-        public RANOEntity RANOGeneral(string start, string end)
+        public MessagesDAL Eventos(string start, string end)
         {
 
-            string jql = string.Format(JQL_GENERAL, start, end);
-            List<Ticket> total_tickets = jiraAccess.GetTiketsIndicadores(jql);
-            List<Ticket> tickets_cerrados = ANIO_CERRADO_A_TIEMPO(total_tickets);
-            return new RANOEntity(tickets_cerrados, total_tickets);
 
         }
     }
