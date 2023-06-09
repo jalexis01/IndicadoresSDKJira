fb9263ce2872b0ac6ac90f70242d19926ba0961e
javieralexisreyes <areyes@eysingenieria.com>
2023-06-09 10:10:46 -0500

    Actualización Dashboard
    Se empieza a pasar información de los indicadores en el dashboard

--- MQTT.Web/Controllers/DashBoardController.cs
+++ MQTT.Web/Controllers/DashBoardController.cs
@@ -1,5 +1,10 @@
+using DashboarJira.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using DashboarJira.Services;
+
 
 namespace MQTT.Web.Controllers
 {
@@ -10,5 +15,40 @@ namespace MQTT.Web.Controllers
         {
             return View();
         }
+
+        public List<IndicadoresEntity> getIndicadores(string startDate, string endDate)
+        {
+            try
+            {
+                string formattedStartDate;
+                string formattedEndDate;
+
+                if (startDate != null || endDate != null)
+                {
+                    //max = 10;
+                    DateTime startDateTime = DateTime.Parse(startDate);
+                    DateTime endDateTime = DateTime.Parse(endDate).AddDays(1).AddSeconds(-1); //agrega 1 día y resta 1 segundo para obtener el final del día
+
+                    formattedStartDate = startDateTime.ToString("yyyy-MM-dd");
+                    formattedEndDate = endDateTime.ToString("yyyy-MM-dd");
+                }
+                else
+                {
+                    formattedStartDate = startDate;
+                    formattedEndDate = endDate;
+                }
+
+                Indicadores indicadores = new Indicadores();
+
+                return indicadores.indicadores(formattedStartDate, formattedEndDate);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
     }
+
 }
--- MQTT.Web/wwwroot/js/Views/Dashboard/dashboard.js
+++ MQTT.Web/wwwroot/js/Views/Dashboard/dashboard.js
@@ -1,107 +1,247 @@
-
 var dashboard;
 var listComponents;
 var rangeData;
 var gauge;
 var lineGauge;
-var fechaActual= new Date(Date.now());
+var fechaActual = new Date(Date.now());
 var datepickerMonth
 var treeGridSummary
+var indicadoresJson;
+var transformedData
+
+
+function ServiceGetIndicadores() {
+    var startDate = '2023-05-01';
+    var endDate = '2023-05-25';
+    console.log("Fecha inicial: " + startDate);
+    console.log("Fecha final: " + endDate);
+
+    Swal.fire({
+        title: 'Cargando indicadores...',
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        onBeforeOpen: (modal) => {
+            modal.showLoading();
+            modal.disableCloseButton();
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "/Indicadores/GetIndicadores",
+        data: { startDate: startDate, endDate: endDate },
+        success: function (response) {
+            Swal.close();
+            var indicadores = [];
+
+            $.each(response, function (index, indicador) {
+                indicadorJson = {
+                    nombre: indicador.nombre,
+                    calculo: indicador.calculo,
+                };
+                indicadores.push(indicadorJson);
+                transformedData = transformData(indicadores);
+            });
+            console.log(indicadoresJson)
+            indicadoresJson = JSON.stringify(indicadores);
+            console.log(indicadoresJson)
+            // Aquí puedes hacer lo que necesites con el objeto indicadoresJson
+            console.log(transformedData);
+            ExtractData(transformedData)
+            treeGridDashBoard(transformedData)
+        },
+        error: function (xhr, status, error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error
+            });
+        }
+    });
+}
+
+
+function transformData(data) {
+    var result = [
+        {
+            id: 1,
+            title: "Indicadores",
+            description: "Aquí están todos los indicadores",
+            childs: []
+        }
+    ];
+
+    var generalChilds = [];
+    var IAIOChilds = [];
+    var IANOChilds = [];
+    var RAIOChilds = [];
+    var RANOChilds = [];
+    var RANOChilds = [];
+    var RANOChilds = [];
+
+    for (var i = 0; i < data.length; i++) {
+        var indicador = data[i];
+
+        if (indicador.nombre.includes("IAIO")) {
+            generalChilds.push({
+                id: i + 2,
+                idFather: 1,
+                title: indicador.nombre,
+                description: "child data primary",
+                value: indicador.calculo,
+                childs: []
+            });
+        } else if (indicador.nombre.includes("IANO")) {
+            generalChilds.push({
+                id: i + 2,
+                idFather: 1,
+                title: indicador.nombre,
+                description: "child data primary",
+                value: indicador.calculo,
+                childs: []
+            });
+        } else if (indicador.nombre.includes("RAIO")) {
+            generalChilds.push({
+                id: i + 2,
+                idFather: 1,
+                title: indicador.nombre,
+                description: "child data primary",
+                value: indicador.calculo,
+                childs: []
+            });
+        } else if (indicador.nombre.includes("RAIO")) {
+            generalChilds.push({
+                id: i + 2,
+                idFather: 1,
+                title: indicador.nombre,
+                description: "child data primary",
+                value: indicador.calculo,
+                childs: []
+            });
+        } else {
+            generalChilds.push({
+                id: i + 2,
+                idFather: 1,
+                title: indicador.nombre,
+                description: "child data primary",
+                value: indicador.calculo,
+                childs: []
+            });
+        }
+    }
+
+    result[0].childs.push(
+        {
+            id: 2,
+            idFather: 1,
+            title: "General",
+            description: "child data primary",
+            value: "",
+            childs: generalChilds
+        }
+    );
+
+    return result;
+}
+
+
+
+
+
 
 var dataTest = [
     {
         id: 1,
-        title:"Principal-data",
-        description:"data primary",
-        value:"80",
+        title: "Indicadores",
+        description: "Aqui estan todos los indicadores",
         childs: [
             {
                 id: 2,
                 idFather: 1,
-                title:"Test-data-01",
-                description:"child data primary",
-                value:"40",
+                title: "Indicador",
+                description: "child data primary",
+                value: "40",
                 childs: [
                     {
                         id: 21,
                         idFather: 2,
-                        title:"Test-second-0101",
-                        description:"child data secondary",
-                        value:"90",
+                        title: "Test-second-0101",
+                        description: "child data secondary",
+                        value: "90",
                     },
                     {
                         id: 22,
                         idFather: 2,
-                        title:"Test-second-0102",
-                        description:"child data secondary",
-                        value:"5",
+                        title: "Test-second-0102",
+                        description: "child data secondary",
+                        value: "5",
                     },
                     {
                         id: 23,
                         idFather: 2,
-                        title:"Test-second-0103",
-                        description:"child data secondary",
-                        value:"12",
+                        title: "Test-second-0103",
+                        description: "child data secondary",
+                        value: "12",
                     }
                 ]
             },
             {
                 id: 3,
                 idFather: 1,
-                title:"Test-data-02",
-                description:"child data primary",
-                value:"80",
+                title: "Indicador B",
+                description: "child data primary",
+                value: "80",
                 childs: [
                     {
                         id: 31,
                         idFather: 3,
-                        title:"Test-second-0201",
-                        description:"child data secondary",
-                        value:"25",
+                        title: "Test-second-0201",
+                        description: "child data secondary",
+                        value: "25",
                     },
                     {
                         id: 32,
                         idFather: 3,
-                        title:"Test-second-0202",
-                        description:"child data secondary",
-                        value:"70",
+                        title: "Test-second-0202",
+                        description: "child data secondary",
+                        value: "70",
                     },
                     {
                         id: 33,
                         idFather: 3,
-                        title:"Test-second-0203",
-                        description:"child data secondary",
-                        value:"100",
+                        title: "Test-second-0203",
+                        description: "child data secondary",
+                        value: "100",
                     }
                 ]
             },
             {
-                id: 4,
+                id: 6,
                 idFather: 1,
-                title:"Test-data-03",
-                description:"child data primary",
-                value:"10",
+                title: "Indicador C",
+                description: "child data primary",
+                value: "25",
                 childs: [
                     {
                         id: 41,
                         idFather: 4,
-                        title:"Test-second-0301",
-                        description:"child data secondary",
-                        value:"16",
+                        title: "Test-second-0301",
+                        description: "child data secondary",
+                        value: "16",
                     },
                     {
                         id: 42,
                         idFather: 4,
-                        title:"Test-second-0302",
-                        description:"child data secondary",
-                        value:"89",
+                        title: "Test-second-0302",
+                        description: "child data secondary",
+                        value: "89",
                     },
                     {
                         id: 43,
                         idFather: 4,
-                        title:"Test-second-0303",
-                        description:"child data secondary",
-                        value:"41",
+                        title: "Test-second-0303",
+                        description: "child data secondary",
+                        value: "41",
                     }
                 ]
             },
@@ -110,80 +250,75 @@ var dataTest = [
 ]
 
 
-$(document).ready(function(){
-    ExtractData(dataTest);
-    treeGridDashBoard(dataTest)
+$(document).ready(function () {
+    console.log(transformedData)
+    //ExtractData(dataTest);
+    //treeGridDashBoard(dataTest)
     SelectorMonth();
 });
 
-function ExtractData(data){
+function ExtractData(data) {
+    console.log(data)
     var numberData = {
-        header: "header"+data[0].id,
-        head: "head"+data[0].id,
-        diagramBar: "diagramBar"+data[0].id,
-        diagram: "diagram"+data[0].id
+        header: "header" + data[0].id,
+        head: "head" + data[0].id,
+        diagramBar: "diagramBar" + data[0].id,
+        diagram: "diagram" + data[0].id
     }
     var rows = 2;
     panels = [
-        {
-            'sizeX': ej.base.Browser.isDevice ? 1 : 3, 'sizeY': ej.base.Browser.isDevice ? 1 : 2, 'row': 0, 'col': 0,
-            header: '<div class="title" id='+ numberData.header + '>' + data[0].title +'</div>', content: '<div style="height:100%; width:100%" id=' + numberData.head + '></div>'
-        },
-        {
-            'sizeX': ej.base.Browser.isDevice ? 1 : 5, 'sizeY': ej.base.Browser.isDevice ? 1 : 2, 'row': 0, 'col': 3,
-            header: '<div class="title" id='+ numberData.diagramBar + '>'+ 'grafico detallado de ' + data[0].title +' </div>', content: '<div  style="height:100%; width:100%" id=' + numberData.diagram + '></div>'
-        },
+
     ]
 
-    data[0]['childs'].forEach(x => { 
+    data[0]['childs'].forEach(x => {
         panels = setPanels(panels, x, rows)
         rows += 2;
-    });    
+    });
     setDashBoard(panels);
     GaugeChartDiagram(data[0]);
     setDiagramPrimary(data[0]);
-    data[0]['childs'].forEach(x => { 
+    data[0]['childs'].forEach(x => {
         GaugeChartDiagram(x);
         setDiagramPrimary(x);
     })
 }
 
-function setPanels(panels, data, row){
+function setPanels(panels, data, row) {
     var numberData = {
-        header: "header"+data.id,
-        head: "head"+data.id,
-        diagramBar: "diagramBar"+data.id,
-        diagram: "diagram"+data.id
+        header: "header" + data.id,
+        head: "head" + data.id,
+        diagramBar: "diagramBar" + data.id,
+        diagram: "diagram" + data.id
     }
     arrayPanel = [
         {
             'sizeX': ej.base.Browser.isDevice ? 1 : 3, 'sizeY': ej.base.Browser.isDevice ? 1 : 2, 'row': row, 'col': 0,
-            header: '<div class="title" id='+ numberData.header + '>' + data.title +'</div>', content: '<div style="height:100%; width:100%" id=' + numberData.head + '></div>'
+            header: '<div class="title" id=' + numberData.header + '>' + data.title + '</div>', content: '<div style="height:100%; width:100%" id=' + numberData.head + '></div>'
         },
         {
             'sizeX': ej.base.Browser.isDevice ? 1 : 5, 'sizeY': ej.base.Browser.isDevice ? 1 : 2, 'row': row, 'col': 3,
-            header: '<div class="title" id='+ numberData.diagramBar + '>'+ 'grafico detallado de ' + data.title +' </div>', content: '<div  style="height:100%; width:100%" id=' + numberData.diagram + '></div>'
+            header: '<div class="title" id=' + numberData.diagramBar + '>' + 'Grafico detallado de ' + data.title + ' </div>', content: '<div  style="height:100%; width:100%" id=' + numberData.diagram + '></div>'
         },
     ];
 
     return panels.concat(arrayPanel);
 }
-  
+
 
 function setDashBoard(data) {
     var dashboard = new ej.layouts.DashboardLayout({
         cellSpacing: [15, 15],
         cellAspectRatio: ej.base.Browser.isDevice ? 1 : 0.8,
         columns: ej.base.Browser.isDevice ? 2 : 8,
-       
+
         panels: data
-    
+
     });
     dashboard.appendTo('#defaultLayout');
 };
 
-function GaugeChartDiagram(data){
-    var mainChart = new  ej.circulargauge.CircularGauge({
+function GaugeChartDiagram(data) {
+    var mainChart = new ej.circulargauge.CircularGauge({
         axes: [{
             lineStyle: { width: 10, color: 'transparent' },
             labelStyle: {
@@ -194,14 +329,14 @@ function GaugeChartDiagram(data){
                 content: '<div><span style="font-size:14px; font-family:Segoe UI">Porcentaje</span></div>',
                 radius: '30%', angle: 0, zIndex: 1
             }, {
-                content: '<div><span style="font-size:20px; font-family:Segoe UI">'+ data.value +'%</span></div>',
+                content: '<div><span style="font-size:20px; font-family:Segoe UI">' + data.value + '%</span></div>',
                 radius: '40%', angle: 180, zIndex: 1
             }],
             startAngle: 210, endAngle: 150, minimum: 0, maximum: 100, radius: '80%',
-            ranges: [{ start: 0, end: 35, color: '#30B32D' }, { start: 35, end: 70, color: '#FFDD00' },
-            { start: 70, end: 100, color: '#F03E3E' }],
+            ranges: [{ start: 0, end: 59, color: '#d11010' }, { start: 60, end: 69, color: '#FFDD00' },
+            { start: 70, end: 79, color: '#ea7e12' }, { start: 80, end: 89, color: '#00b300' }, { start: 90, end: 100, color: '#00ff00' }],
             pointers: [{
-                value: data.value, radius: data.value + '%', pointerWidth: 8,
+                value: data.value, radius: 100 + '%', pointerWidth: 8,
                 cap: { radius: 7 }, needleTail: { length: '18%' }
             }]
         }],
@@ -215,10 +350,10 @@ function GaugeChartDiagram(data){
         },
 
     });
-      mainChart.appendTo("#head" + data.id);
+    mainChart.appendTo("#head" + data.id);
 }
 
-function setDiagramPrimary(data){
+function setDiagramPrimary(data) {
     var subChart1 = new ej.charts.Chart({
         primaryXAxis: {
             valueType: 'Category',
@@ -238,7 +373,7 @@ function setDiagramPrimary(data){
         tooltip: {
             enable: true
         },
-        legendSettings: { enableHighlight :true },
+        legendSettings: { enableHighlight: true },
         series: [
             {
                 type: 'Column',
@@ -249,14 +384,14 @@ function setDiagramPrimary(data){
                 width: "200px"
             }
         ]
-      });
-      subChart1.appendTo("#diagram" + data.id);
+    });
+    subChart1.appendTo("#diagram" + data.id);
 }
 
 
-function SelectorMonth(){
+function SelectorMonth() {
     datepickerMonth = new ej.calendars.DatePicker({
-        start: 'Year', 
+        start: 'Year',
         depth: 'Year',
         format: 'MMMM y',
         renderDayCell: disableDate,
@@ -266,12 +401,12 @@ function SelectorMonth(){
 }
 
 function disableDate(args) {
-    if (args.date.getMonth() > fechaActual.getMonth()){
+    if (args.date.getMonth() > fechaActual.getMonth()) {
         args.isDisabled = true;
     }
 }
 
-function treeGridDashBoard(data){
+function treeGridDashBoard(data) {
     var treeGridObj = new ej.treegrid.TreeGrid({
         dataSource: data,
         childMapping: 'childs',
@@ -281,7 +416,6 @@ function treeGridDashBoard(data){
         columns: [
             { field: 'id', headerText: 'ID', width: 70, textAlign: 'Right' },
             { field: 'title', headerText: 'Nombre', width: 200, textAlign: 'Left' },
-            { field: 'description', headerText: 'Descricion', width: 200, textAlign: 'Left'},
             { field: 'value', headerText: 'Valor', width: 90, textAlign: 'Left' },
         ]
     });
