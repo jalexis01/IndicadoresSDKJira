2799f82b9bb9130db1a19d492f26dc3352742519
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-06-15 09:15:58 -0500

    change the consult

--- MQTT.FunctionApp/Models/Filters.cs
+++ MQTT.FunctionApp/Models/Filters.cs
@@ -111,7 +111,7 @@ namespace MQTT.FunctionApp.Models
             {
                 resultQuery += $" AND key = {this.id_ticket}";
             }
-            resultQuery += " &maxResults=" + Int32.MaxValue;
+            resultQuery += " AND 'Descripcion de la reparacion' is not empty " +  " & maxResults = " + Int32.MaxValue;
 
             return results;
         }
