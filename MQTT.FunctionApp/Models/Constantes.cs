ed29b902122419526e1c8b3033ff74226a4ce68a
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-07-12 17:50:12 -0500

    idnk

--- MQTT.FunctionApp/Models/Constantes.cs
+++ MQTT.FunctionApp/Models/Constantes.cs
@@ -6,30 +6,30 @@ namespace MQTT.FunctionApp.Models
 {
     public class Constantes
     {
-        public const string Estacion = "customfield_10057";
-        public const string Vagon = "customfield_10058";
-        public const string IdentificacionComponente = "customfield_10060";
-        public const string TipoDeComponente = "customfield_10088";
-        public const string IdentificacionSerial = "customfield_10059";
-        public const string TipoDeServicio = "customfield_10061";
-        public const string ClaseDeFallo = "customfield_10064";
-        public const string DescripcionDeFallo = "customfield_10069";
-        public const string Created = "created";
-        public const string StatusCategoryChangeDate = "statuscategorychangedate";
-        public const string FechayHoraDeLlegadaAEstacion = "customfield_10071";
-        public const string DescripcionRepuesto = "customfield_10072";
-        public const string TipoReparacion = "customfield_10081";
-        public const string ListadoAjustesPuerta = "customfield_10075";
-        public const string ListadoConfiguracionPuerta = "customfield_10076";
-        public const string ListadoAjustesITS = "customfield_10077";
-        public const string ListadoConfiguracionITS = "customfield_10078";
-        public const string ListadoConfiguracionRFID = "customfield_10086";
-        public const string DescripcionReparacion = "customfield_10105";
-        public const string DiagnosticoCausa = "customfield_10104";
-        public const string TipoCausa = "customfield_10067";
-        public const string FechaSolucion = "customfield_10101";
-        public const string Status = "Status";
+        public string Estacion = "customfield_10057";
+        public string Vagon = "customfield_10058";
+        public string IdentificacionComponente = "customfield_10060";
+        public string TipoDeComponente = "customfield_10088";
+        public string IdentificacionSerial = "customfield_10059";
+        public string TipoDeServicio = "customfield_10061";
+        public string ClaseDeFallo = "customfield_10064";
+        public string DescripcionDeFallo = "customfield_10069";
+        public string Created = "created";
+        public string StatusCategoryChangeDate = "statuscategorychangedate";
+        public string FechayHoraDeLlegadaAEstacion = "customfield_10071";
+        public string DescripcionRepuesto = "customfield_10072";
+        public string TipoReparacion = "customfield_10081";
+        public string ListadoAjustesPuerta = "customfield_10075";
+        public string ListadoConfiguracionPuerta = "customfield_10076";
+        public string ListadoAjustesITS = "customfield_10077";
+        public string ListadoConfiguracionITS = "customfield_10078";
+        public string ListadoConfiguracionRFID = "customfield_10086";
+        public string DescripcionReparacion = "customfield_10105";
+        public string DiagnosticoCausa = "customfield_10104";
+        public string TipoCausa = "customfield_10067";
+        public string FechaSolucion = "customfield_10101";
+        public string Status = "Status";
 
-        public const string URI = "https://manateecc.atlassian.net/rest/api/2";
+        public string URI = "https://manateecc.atlassian.net/rest/api/2";
     }
 }
