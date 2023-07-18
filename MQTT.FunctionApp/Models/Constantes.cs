3b37c2a4b2547a49160726f9d04b33622bc474d2
Juan Andres Orozco <juanxxi2015@gmail.com>
2023-07-18 14:09:14 -0500

    Revert "Update Constantes to static values"
    This reverts commit aa32ffd94a7c55fcae48cff249692c96139b9183.

--- MQTT.FunctionApp/Models/Constantes.cs
+++ MQTT.FunctionApp/Models/Constantes.cs
@@ -4,32 +4,32 @@ using System.Text;
 
 namespace MQTT.FunctionApp.Models
 {
-    public static class Constantes
+    public class Constantes
     {
-        public static string Estacion = "customfield_10057";
-        public static string Vagon = "customfield_10058";
-        public static string IdentificacionComponente = "customfield_10060";
-        public static string TipoDeComponente = "customfield_10088";
-        public static string IdentificacionSerial = "customfield_10059";
-        public static string TipoDeServicio = "customfield_10061";
-        public static string ClaseDeFallo = "customfield_10064";
-        public static string DescripcionDeFallo = "customfield_10069";
-        public static string Created = "created";
-        public static string StatusCategoryChangeDate = "statuscategorychangedate";
-        public static string FechayHoraDeLlegadaAEstacion = "customfield_10071";
-        public static string DescripcionRepuesto = "customfield_10072";
-        public static string TipoReparacion = "customfield_10081";
-        public static string ListadoAjustesPuerta = "customfield_10075";
-        public static string ListadoConfiguracionPuerta = "customfield_10076";
-        public static string ListadoAjustesITS = "customfield_10077";
-        public static string ListadoConfiguracionITS = "customfield_10078";
-        public static string ListadoConfiguracionRFID = "customfield_10086";
-        public static string DescripcionReparacion = "customfield_10105";
-        public static string DiagnosticoCausa = "customfield_10104";
-        public static string TipoCausa = "customfield_10067";
-        public static string FechaSolucion = "customfield_10101";
-        public static string Status = "Status";
+        public string Estacion = "customfield_10057";
+        public string Vagon = "customfield_10058";
+        public string IdentificacionComponente = "customfield_10060";
+        public string TipoDeComponente = "customfield_10088";
+        public string IdentificacionSerial = "customfield_10059";
+        public string TipoDeServicio = "customfield_10061";
+        public string ClaseDeFallo = "customfield_10064";
+        public string DescripcionDeFallo = "customfield_10069";
+        public string Created = "created";
+        public string StatusCategoryChangeDate = "statuscategorychangedate";
+        public string FechayHoraDeLlegadaAEstacion = "customfield_10071";
+        public string DescripcionRepuesto = "customfield_10072";
+        public string TipoReparacion = "customfield_10081";
+        public string ListadoAjustesPuerta = "customfield_10075";
+        public string ListadoConfiguracionPuerta = "customfield_10076";
+        public string ListadoAjustesITS = "customfield_10077";
+        public string ListadoConfiguracionITS = "customfield_10078";
+        public string ListadoConfiguracionRFID = "customfield_10086";
+        public string DescripcionReparacion = "customfield_10105";
+        public string DiagnosticoCausa = "customfield_10104";
+        public string TipoCausa = "customfield_10067";
+        public string FechaSolucion = "customfield_10101";
+        public string Status = "Status";
 
-        public static string URI = "https://manateecc.atlassian.net/rest/api/2";
+        public string URI = "https://manateecc.atlassian.net/rest/api/2";
     }
 }
