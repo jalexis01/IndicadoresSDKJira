47247604920f6a148dd7d5a4d41366987c58c00a
Juan Andres Orozco <juanxxi2015@gmail.com>
2023-07-18 08:49:18 -0500

    UNcomment to check if file exists or dont

--- DashboarJira/Program.cs
+++ DashboarJira/Program.cs
@@ -79,5 +79,6 @@ using (FileStream archivo = new FileStream(rutaCompleta, FileMode.Create))
 }
 */
 Console.WriteLine("Archivo creado exitosamente.");
+byte[] bytes = jira.getIssueJira("TICKET-100").Archivos;
 
 //jira.GetTikets(0, 0, fechainicio, fechaFinal, null);
