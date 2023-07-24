0185f0cd4f980937d4574f7f45803821b101417a
Juan Andres Orozco <aorozco@eysingenieria.com>
2023-07-24 14:00:50 -0500

    Updated Assaabloy

--- MQTT.FunctionApp/GetIssuesJira.cs
+++ MQTT.FunctionApp/GetIssuesJira.cs
@@ -32,12 +32,12 @@ namespace MQTT.FunctionApp
 			var logRequestIn = new Infrastructure.Models.DTO.LogRequestInDTO();
 			logRequestIn.IdEndPoint = (int)EndPointEnum.GetIssueJira;
 
-            var connectionString = Environment.GetEnvironmentVariable("ConnectionStringDB", EnvironmentVariableTarget.Process);
-            string token = Environment.GetEnvironmentVariable("TokenJira", EnvironmentVariableTarget.Process).ToString();
-            string timeZone = Environment.GetEnvironmentVariable("TimeZone", EnvironmentVariableTarget.Process).ToString();
-            //var connectionString = "Server=manatee.database.windows.net;Database=PuertasTransmilenioDB;User Id=administrador;Password=2022/M4n4t334zur3;";
-            //string token = "ZGVzYXJyb2xsb2NjQG1hbmF0ZWVpbmdlbmllcmlhLmNvbTpoZlV0Z1o5UkZHb1F5MlNmSDdzQ0Y5QTY=";
-            //string timeZone = "SA Pacific Standard Time";
+            //var connectionString = Environment.GetEnvironmentVariable("ConnectionStringDB", EnvironmentVariableTarget.Process);
+            //string token = Environment.GetEnvironmentVariable("TokenJira", EnvironmentVariableTarget.Process).ToString();
+            //string timeZone = Environment.GetEnvironmentVariable("TimeZone", EnvironmentVariableTarget.Process).ToString();
+            var connectionString = "Server=manatee.database.windows.net;Database=PuertasTransmilenioDB;User Id=administrador;Password=2022/M4n4t334zur3;";
+            string token = "ZGVzYXJyb2xsb2NjQG1hbmF0ZWVpbmdlbmllcmlhLmNvbTpoZlV0Z1o5UkZHb1F5MlNmSDdzQ0Y5QTY=";
+            string timeZone = "SA Pacific Standard Time";
             General DBAccess = new General(connectionString);
 			 
 			try
@@ -262,7 +262,7 @@ namespace MQTT.FunctionApp
 
             if (fieldObject.TryGetValue(Constantes.IdentificacionSerial, out JToken IdentificacionSerial) && IdentificacionSerial.Type != JTokenType.Null)
             {
-                field.IdentificacionSerial = fieldObject["customfield_10059"].Value<string>();
+                field.IdentificacionSerial = fieldObject[Constantes.IdentificacionSerial].Value<string>();
             }
             else
             {
@@ -467,7 +467,8 @@ namespace MQTT.FunctionApp
 
             if (fieldObject.TryGetValue(Constantes.DiagnosticoCausa, out JToken DiagnosticoCausa) && DiagnosticoCausa.Type != JTokenType.Null)
             {
-                field.DiagnosticoCausa = fieldObject["customfield_10104"].Value<string>();
+                
+                field.DiagnosticoCausa = fieldObject[Constantes.DiagnosticoCausa].Value<string>();
             }
             else
             {
