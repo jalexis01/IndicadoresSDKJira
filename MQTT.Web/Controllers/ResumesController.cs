b4f56d8f2dcf857a154d07e9c06fd45907608e6e
javieralexisreyes <areyes@eysingenieria.com>
2023-08-11 11:11:23 -0500

    Se agrega MVC para ver hojas de vida

---
+++ MQTT.Web/Controllers/ResumesController.cs
@@ -0,0 +1,109 @@
+﻿using DashboarJira.Model;
+using DashboarJira.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MQTT.Web.Controllers
+{
+    [Authorize]
+    public class ResumesController : Controller
+    {
+        public IActionResult Index(int max, string componente)
+        {
+            // Obtiene la identidad del usuario actual
+            var identity = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            // Verifica si el usuario tiene el rol de "Administrador"
+            if (identity != null && identity.HasClaim(System.Security.Claims.ClaimTypes.Name, "admin@admin.com"))
+            {
+                ViewBag.Menu = "admin";
+            }
+            else
+            {
+                ViewBag.Menu = "user";
+            }
+            return View();
+        }
+
+        int start = 0;
+
+        public List<Ticket> getTickets(string startDate, string endDate, int max, string componente)
+        {
+            try
+            {
+                string formattedStartDate;
+                string formattedEndDate;
+
+                if (startDate != null || endDate != null)
+                {
+                    //max = 10;
+                    DateTime startDateTime = DateTime.Parse(startDate);
+                    DateTime endDateTime = DateTime.Parse(endDate).AddDays(1); //agrega 1 día y resta 1 segundo para obtener el final del día
+
+                    formattedStartDate = startDateTime.ToString("yyyy-MM-dd");
+                    formattedEndDate = endDateTime.ToString("yyyy-MM-dd");
+                }
+                else
+                {
+                    formattedStartDate = startDate;
+                    formattedEndDate = endDate;
+                }
+
+                JiraAccess jiraAccess = new JiraAccess();
+                max = 0;
+                return jiraAccess.GetTikets(start, max, formattedStartDate, formattedEndDate, componente);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public IActionResult consultarTicket(string idTicket)
+        {
+            try
+            {
+                JiraAccess jira = new JiraAccess();
+                IssueJira ticket = jira.getIssueJira(idTicket);
+                return Ok(ticket);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public IActionResult GetImageTicket(string idTicket)
+        {
+            try
+            {
+                JiraAccess jira = new JiraAccess();
+                List<byte[]> images = jira.GetAttachmentImages(idTicket);
+
+                if (images.Count > 0)
+                {
+                    List<string> base64Images = new List<string>();
+
+                    foreach (byte[] imageData in images)
+                    {
+                        string base64Image = Convert.ToBase64String(imageData);
+                        base64Images.Add(base64Image);
+                    }
+
+                    return Ok(base64Images);
+                }
+                else
+                {
+                    return NotFound(); // or return some appropriate response when no images are found
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message); // or handle the exception in an appropriate way
+            }
+        }
+
+    }
+}
---
+++ MQTT.Web/wwwroot/js/Views/Resumes/resumes.js
@@ -0,0 +1,208 @@
+﻿var dataHideColums, dataSearchColumns;
+$(document).ready(function () {
+    createElemntsTimes();
+    multiSelect();
+    drodownDataSearch(columnsSearch, 'CustomName', 'searchParam');
+});
+
+function validateDates() {
+    var startDate = $('#dtpStart').val();
+    var endDate = $('#dtpEnd').val();
+    console.log("Fecha inicial: " + startDate);
+    console.log("Fecha final: " + endDate);
+
+    if (startDate === "" || endDate === "") {
+        Swal.fire({
+            title: 'Debe seleccionar la fecha',
+        });
+    } else {
+        ServiceGetMessages();
+    }
+}
+
+
+function getImageTicket(idTicket) {
+    $.ajax({
+        url: '/Tickets/getImageTicket?idTicket=' + idTicket,
+        data: { idTicket: idTicket },
+        success: function (base64Images) {
+            if (base64Images && base64Images.length > 0) {
+                var imageContainer = document.createElement('div');
+                imageContainer.style.display = 'flex';
+                imageContainer.style.flexWrap = 'wrap';
+                imageContainer.style.justifyContent = 'center';
+                // Iterate through the list of base64 images and create image elements
+                for (var i = 0; i < base64Images.length; i++) {
+                    var base64Image = base64Images[i];
+                    var imageElement = document.createElement('img');
+                    imageElement.src = 'data:image/jpeg;base64,' + base64Image;
+                    imageElement.style.width = '90%'; // Adjust the width as needed
+                    imageElement.style.margin = '10px';
+                    imageContainer.appendChild(imageElement);
+                }
+
+                // Show the image container in the Swal dialog
+                Swal.fire({
+                    title: 'Imágenes del ' + idTicket,
+                    html: imageContainer,
+                    confirmButtonText: 'Cerrar',
+                    showCloseButton: true,
+                    showConfirmButton: true,
+                    customClass: {
+                        container: 'swal-wide',
+                    },
+                    width: '80%', // Adjust the width as needed
+                    padding: '2rem',
+                    backdrop: true,
+                    allowOutsideClick: true,
+                    allowEscapeKey: false,
+                });
+            } else {
+                Swal.fire('Información', 'El ticket no tiene imágenes adjuntas', 'info');
+            }
+        },
+        error: function () {
+            Swal.fire('Error', 'El ticket no tiene imágenes', 'error');
+        }
+    });
+}
+function showMoreInformation(idTicket) {
+    $.ajax({
+        url: '/Tickets/consultarTicket?idTicket=' + idTicket,
+        data: { idTicket: idTicket },
+        success: function (ticket) {
+
+            var html = '<table style="font-family: Arial, sans-serif; width: 100%; border-collapse: collapse;">';
+
+            $.each(ticket, function (key, value) {
+
+                html += '<tr>';
+                html += '<td style="text-align: left; padding: 8px; border: 2px solid #7f69a5; white-space: nowrap; font-weight: bold; font-size: 14px; background-color: rgba(127, 105, 165, 0.3);">' + key + ':</td>';
+                html += '<td style="text-align: left; padding: 8px; border: 2px solid #7f69a5; font-size: 13px;">' + value + '</td>';
+                html += '</tr>';
+            });
+
+            html += '</table>';
+
+            Swal.fire({
+                title: 'Información del Ticket',
+                html: html,
+                confirmButtonText: 'Cerrar',
+                showCloseButton: true,
+                showConfirmButton: false,
+                customClass: {
+                    container: 'swal-wide',
+                },
+                width: '50%',
+                padding: '2rem',
+                backdrop: true,
+                allowOutsideClick: true,
+                allowEscapeKey: false,
+            });
+        },
+        error: function () {
+
+        }
+    });
+}
+
+
+
+function showMoreInformationTickets(idTicket) {
+    let ticketToReturn
+    $.ajax({
+        url: '/Tickets/consultarTicket?idTicket=' + idTicket,
+        data: { idTicket: idTicket },
+    }).then(response => JSON.parse(JSON.stringify(response)))
+        .then(data => {
+            console.log("Ticket data: " + data)
+            if (data.length == 0) {
+                noData();
+                return;
+            } else {
+                ticketToReturn = setColums(data, null);
+                return Ok(ticketToReturn);
+            }
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.name + ': ' + error.message
+            });
+        })
+        .then(response => console.log('Success:', response));
+}
+
+
+
+/********************************* */
+function ServiceGetMessages() {
+    var startDate = $('#dtpStart').val();
+    var endDate = $('#dtpEnd').val();
+    var max = 0;
+    var componente = $('#componente').val();
+    console.log("idComponente: " + componente);
+    console.log("Max: " + max);
+    Swal.fire({
+        title: 'Cargando...',
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        onBeforeOpen: (modal) => {
+            modal.showLoading();
+            modal.disableCloseButton();
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: "/Tickets/GetTickets",
+        data: { startDate: startDate, endDate: endDate, max: max, componente: componente },
+
+    }).then(response => JSON.parse(JSON.stringify(response)))
+        .then(data => {
+
+            Swal.close();
+            console.log(data)
+            if (data.length == 0) {
+                noData();
+                return;
+            } else {
+                let dataColumns = setColums(data, null);
+                let exportFunctions = addFnctionsGrid(['Excel']);
+
+                dataColumns = addCommandsGridDetails(dataColumns);
+                dataGridSave = data;
+                setGrid(data, dataColumns, exportFunctions);
+            }
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.name + ': ' + error.message
+            });
+        })
+        .then(response => console.log('Success:', response));
+}
+
+
+
+const targetEl = document.getElementById('dropdownInformation');
+
+// set the element that trigger the dropdown menu on click
+const triggerEl = document.getElementById('dropdownInformationButton');
+
+// options with default values
+const options = {
+    placement: 'bottom',
+    onHide: () => {
+        console.log('dropdown has been hidden');
+    },
+    onShow: () => {
+        console.log('dropdown has been shown');
+    }
+};
+
+var detailsData = function (args) {
+    //alert(JSON.stringify(args.rowData));
+}
