6bdda316ca598f3c8b8bd8a16e21a0c02cb2f77f
javieralexisreyes <areyes@eysingenieria.com>
2023-08-22 16:33:34 -0500

    Actualización contador de videos
    Se coloca el contador de video en el boton de ver video

--- MQTT.Web/wwwroot/js/shared/syncfusion-coponents.js
+++ MQTT.Web/wwwroot/js/shared/syncfusion-coponents.js
@@ -666,18 +666,16 @@ var detailsData = function (args) {
         if (formattedKey == "idTicket") {
             idTicket = value;
             getContImageTicket(idTicket);
+            getContVideoTicket(idTicket);
         }
         
         dataHtmlList += "<ul><li style='padding: 1% 0%;'><div class='flex items-start space-x-4'><div class='flex-1 min-w-0' style='text-align: initial;'><p class='text-sm font-medium text-gray-900 truncate dark:text-white'>" + formattedKey + "</p></div></li><li><div class='flex items-start space-x-4'><div class='flex-1 min-w-0' style='text-align: initial'><p class='text-sm font-sm text-gray-900 truncate dark:text-white'>" + value + "</p></div></li></ul>"
 
-        if (idTicket !== "") {
-
-            
-          //  getContImageTicket(idTicket);
-            
+        if (idTicket !== "") {            
+          //  getContImageTicket(idTicket);            
 
             footerHtml = '<button id="verMasBtn" style="background: linear-gradient(to bottom, #0071A1, #00BFFF); color: white; border: none; border-radius: 4px; padding: 8px 16px; cursor: pointer; font-weight: bold; margin-right: 5px;" onclick="getImageTicket(\'' + idTicket + '\')">Ver imagen (' + cantImagenes + ')</button>' +
-                '<button id="verVideoBtn" style="background: linear-gradient(to bottom right, #ff4d4d, #ff9999); color: white; border: none; border-radius: 4px; padding: 8px 16px; cursor: pointer; font-weight: bold; margin-left: 5px; margin-right: 5px;" onclick="openVideoModal(\'' + idTicket + '\')">Ver video</button>' +
+                '<button id="verVideoBtn" style="background: linear-gradient(to bottom right, #ff4d4d, #ff9999); color: white; border: none; border-radius: 4px; padding: 8px 16px; cursor: pointer; font-weight: bold; margin-left: 5px; margin-right: 5px;" onclick="openVideoModal(\'' + idTicket + '\')">Ver video (' + cantImagenes + ')</button>' +
                 '<button id="cerrarBtn" style="background: linear-gradient(to bottom right, #888888, #555555); color: white; border: none; border-radius: 4px; padding: 8px 16px; cursor: pointer; font-weight: bold; margin-left: 5px; margin-right: 5px;" onclick="closeSwal()">Cerrar</button>';
         }
     }
