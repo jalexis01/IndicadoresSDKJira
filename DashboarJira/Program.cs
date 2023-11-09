d8e0d6adbc5db190815bec67ef79f579a273f2ef
Josué Rodríguez <47890658+Josue2535@users.noreply.github.com>
2023-11-09 17:25:39 -0500

    Try to get all HVs

--- DashboarJira/Program.cs
+++ DashboarJira/Program.cs
@@ -19,30 +19,59 @@ DbConnector dbConnector = new DbConnector();
 JiraAccess jira = new JiraAccess();
 var fechainicio = "2023-10-01";
 var fechaFinal = "2023-11-02";
-//List<ComponenteHV> listaIdComponentes = dbConnector.GetComponentesHV();
+List<ComponenteHV> listaIdComponentes = dbConnector.GetComponentesHV();
+string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.FullName;
+string logFilePath = Path.Combine(projectDirectory, "ErroresLog.txt");
 
-// Itera sobre cada identificador y llama al método GetComponenteHV
-//foreach (var componente in listaIdComponentes)
-//{
+foreach (var componente in listaIdComponentes)
+{
+    // Puedes hacer algo con el componente obtenido, por ejemplo, imprimir sus propiedades
+    if (componente.IdComponente != null)
+    {
+        try
+        {
+            jira.ExportComponenteToExcel(componente.IdComponente);
+            List<TicketHV> tickets = jira.GetTicketHVs(0, 0, componente.IdComponente);
+            jira.ExportTicketsToExcel(tickets);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al exportar el componente {componente.IdComponente}: {e.Message}");
 
+            // Registra el error en el archivo de log
+            LogError(logFilePath, $"Error al exportar el componente {componente.IdComponente}: {e.ToString()}");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"No se encontró el componente con ID: {componente.IdComponente}");
+    }
+}
 
-//    // Puedes hacer algo con el componente obtenido, por ejemplo, imprimir sus propiedades
-//    if (componente.IdComponente != null)
-//    {
-//        jira.ExportComponenteToExcel(componente.IdComponente);
-//        jira.ExportComponenteToExcel(componente.IdComponente);
-//    }
-//    else
-//    {
-//        Console.WriteLine($"No se encontró el componente con ID: {componente.IdComponente}");
-//    }
-//}
-var componente = "9120-WA-OR-2";
-var ticketsReales = jira.GetTikets(0, 0, null, null, componente);
-List<TicketHV> tickets = jira.GetTicketHVs(0, 0, componente);
-jira.ExportComponenteToExcel(componente);
+// ...
 
-jira.ExportTicketsToExcel(tickets);
+static void LogError(string filePath, string errorMessage)
+{
+    try
+    {
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine($"[{DateTime.Now}] {errorMessage}");
+            writer.WriteLine("------------------------------------------------------------");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error al escribir en el archivo de log: {ex.Message}");
+    }
+}
+
+//var componente = "9120-WA-OR-2";
+//var ticketsReales = jira.GetTikets(0, 0, null, null, componente);
+//List<TicketHV> tickets = jira.GetTicketHVs(0, 0, componente);
+//jira.ExportComponenteToExcel(componente);
+
+//jira.ExportTicketsToExcel(tickets);
 
 // Llamar a la función para obtener la DataTable
 //DataTable dataTable = jira.getEstaciones();
@@ -50,4 +79,4 @@ jira.ExportTicketsToExcel(tickets);
 //List<TicketHV> tickets = jira.GetTicketHVs(0, 0, "9110-WA-OR-2");
 //jira.ExportComponenteToExcel("9120-WA-OR-1");
 //jira.ExportTicketsToExcel(tickets);
-					
+
